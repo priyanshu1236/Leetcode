f78be9f4ed83f8faf4850132c98bf8c24c540f63
priyanshu <152264776+priyanshu1236@users.noreply.github.com>
2026-08-03 23:50:58 +0530

    0240-search-a-2d-matrix-ii Medium 52 ms (65.37%) 18.8 MB (39.46%) .cpp 08-03-2026

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,43 +1,71 @@
-class Solution {
-public:
-    bool binary(int i,int low,int high,vector<vector<int>>& matrix,int target)
-    {
-        while(low<=high)
-        {    int mid=low+(high-low)/2;
-            if(matrix[i][mid]==target)
-            {
-                return true;
-            }
-            else if(matrix[i][mid]>target)
-            {
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return false;
-    }
+//better 
+
+// class Solution {
+// public:
+//     bool binary(int i,int low,int high,vector<vector<int>>& matrix,int target)
+//     {
+//         while(low<=high)
+//         {    int mid=low+(high-low)/2;
+//             if(matrix[i][mid]==target)
+//             {
+//                 return true;
+//             }
+//             else if(matrix[i][mid]>target)
+//             {
+//                 high=mid-1;
+//             }
+//             else{
+//                 low=mid+1;
+//             }
+//         }
+//         return false;
+//     }
 
 
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m=matrix.size();
+//         int n=matrix[0].size();
 
-        for(int i=0;i<m;i++)
-        {
-             int low=0;
-             int high=n-1;
+//         for(int i=0;i<m;i++)
+//         {
+//              int low=0;
+//              int high=n-1;
            
-             bool result=binary(i,low,high,matrix,target);
-             if(result)
-             {
-                return true;
-             }
+//              bool result=binary(i,low,high,matrix,target);
+//              if(result)
+//              {
+//                 return true;
+//              }
              
             
-        }
-        return false;
+//         }
+//         return false;
         
+//     }
+// };
+
+//optimal stair case approch
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+         int m=matrix.size();
+         int n=matrix[0].size();
+        int row=0;
+        int col=n-1;
+        while(row<m && col>=0)
+        {
+            if(matrix[row][col]==target)
+            {
+                return true;
+            }
+            else if(matrix[row][col]>target)
+            {
+                col--;
+            }
+            else
+            row++;
+        }
+
+return false;
     }
 };
